8b03dbe351d82768cb0f31ccaa864b01c2168f0d
Praneet Karna <praneetkarna77@gmail.com>
2021-08-17 14:11:59 +0545

    Complete challenge 1

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+// CHALLENGE - 1
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite foods.</h1>
+    <ul>
+      <li> Chicken </li>
+      <li> Mutton </li>
+      <li> Pork </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
