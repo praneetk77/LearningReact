e9aa30bfb023cdb1abd7513b1340897cb8e79360
Praneet Karna <praneetkarna77@gmail.com>
2021-08-17 14:28:20 +0545

    Add JS expressions in html code

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@
 // ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
 
 // CHALLENGE - 1
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <div>
+//     <h1>My favourite foods.</h1>
+//     <ul>
+//       <li> Chicken </li>
+//       <li> Mutton </li>
+//       <li> Pork </li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// Learning
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fname = "Praneet";
+const lname = "Karna";
+const lucky_number = 2;
+
 ReactDOM.render(
   <div>
-    <h1>My favourite foods.</h1>
-    <ul>
-      <li> Chicken </li>
-      <li> Mutton </li>
-      <li> Pork </li>
-    </ul>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>Your lucky number is {lucky_number}.</p>
+    <p>Your lucky number is not {2 + 1212 - 121}.</p>
   </div>,
   document.getElementById("root")
 );
